240446d511d728fbba8c590afe628ff6d4c7a086
cesar.buffevant <cbuffevant@gmail.com>
2016-03-05 19:53:26 -0300

    Initial commit

---
+++ src/main/java/ar/fiuba/tdd/template/Main.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tdd.template;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is just a template project");
+    }
+}
---
+++ src/test/java/ar/fiuba/tdd/template/MainTests.java
@@ -0,0 +1,13 @@
+package ar.fiuba.tdd.template;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTests {
+
+    @Test
+    public void dummy() {
+        assertEquals(0, 0);
+    }
+}
