0de83477106b3e069bef78ff2691934de22c191f
Juan Manuel Bouvier <juanmanuel.bouvier@adoos.com>
2016-09-18 19:51:30 -0300

    Fixed GridRuleManager for passing checkstyle: added <?> to GridRules.

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/rules/GridRuleManager.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/rules/GridRuleManager.java
@@ -8,32 +8,33 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class GridRuleManager implements OnGridUpdatedObserver {
-    private Collection<GridRule> rules;
+    private Collection<GridRule<?>> rules;
 
     public GridRuleManager() {
         rules = new ArrayList<>();
     }
 
-    public GridRuleManager(Collection<GridRule> rules) {
+    public GridRuleManager(Collection<GridRule<?>> rules) {
         this.rules = rules;
     }
 
-    public boolean addRule(GridRule rule) {
+    public boolean addRule(GridRule<?> rule) {
         return this.rules.add(rule);
     }
 
-    public boolean removeRule(GridRule rule) {
+    public boolean removeRule(GridRule<?> rule) {
         return this.rules.remove(rule);
     }
 
     @Override
     public void onGridUpdated(GameGrid grid) {
         this.rules.forEach(rule -> {
-            try {
-                rule.verifyRule(grid.getCells());
-            } catch (RuleNotSatisfiedException e) {
-                // TODO: 18/09/16 Decide what to do here.
+                try {
+                    rule.verifyRule(grid.getCells());
+                } catch (RuleNotSatisfiedException e) {
+                    // TODO: 18/09/16 Decide what to do here.
+                }
             }
-        });
+        );
     }
 }
