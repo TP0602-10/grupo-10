09543d44bf2506e5227a0c7785933ae5ec38b29d
juanmamanuel.bouvier <Buby24993>
2016-09-20 17:36:19 -0300

    Fixed classes for checkstyle.

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/cells/CellContent.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/cells/CellContent.java
@@ -1,19 +1,16 @@
 package ar.fiuba.tdd.grupo10.nikoligames.grid.cells;
 
-import com.sun.istack.internal.NotNull;
-
 import java.util.Objects;
 
 /**
  * Content of a grid cell.
  * Works as a container for generic values.
- * Value cannot be null! Set the CellContent as null for this purpose instead.
  * @param <T> Type of the content that the cell can have
  */
 public class CellContent<T> {
     private T value;
 
-    public CellContent(@NotNull T value) {
+    public CellContent(T value) {
         this.value = value;
     }
 
@@ -21,15 +18,19 @@ public class CellContent<T> {
         return value;
     }
 
-    public void setValue(@NotNull T value) {
+    public void setValue(T value) {
         this.value = value;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CellContent<?> that = (CellContent<?>) o;
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        CellContent<?> that = (CellContent<?>) obj;
         return Objects.equals(value, that.value);
     }
 
--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/rules/GridRuleIteratorHelper.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/rules/GridRuleIteratorHelper.java
@@ -6,9 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Helper class for auxiliary uses of the GridRuleIterator@{@link GridRuleIterator}
+ * Helper class for auxiliary uses of the GridRuleIterator@{@link GridRuleIterator}.
  */
 public class GridRuleIteratorHelper {
+
     public static List<GridCell> listAllCells(GridRuleIterator iterator) {
         List<GridCell> allCells = new ArrayList<>();
         while (iterator.hasNext()) {
@@ -17,4 +18,5 @@ public class GridRuleIteratorHelper {
         }
         return allCells;
     }
+
 }
