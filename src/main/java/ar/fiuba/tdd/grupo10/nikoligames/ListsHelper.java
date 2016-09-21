482e06815889e5e15e3712638d0000791a38d976
Juan Manuel Bouvier <juanmanuel.bouvier@adoos.com>
2016-09-21 19:22:04 -0300

    Fixed type warnings in ListsHelper.

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/ListsHelper.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/ListsHelper.java
@@ -7,13 +7,13 @@ import java.util.stream.Collectors;
  * Helper for simplifying the common operations of generic lists.
  */
 public class ListsHelper {
-    public static boolean equals(List<?> list1, List<?> list2) {
+    public static <T> boolean equals(List<T> list1, List<T> list2) {
         return list1.size() == list2.size()
-                && list1.contains(list2)
-                && list2.contains(list1);
+                && list1.containsAll(list2)
+                && list2.containsAll(list1);
     }
 
-    public static List<?> rejectDuplicateElements(List<?> list) {
+    public static <T> List<T> rejectDuplicateElements(List<T> list) {
         return list.stream().distinct().collect(Collectors.toList());
     }
 }
