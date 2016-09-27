a34dc33c4b4a5592003c9a0298a5e582fad71b4d
martin <martin@magnetico.com.ar>
2016-09-27 12:23:29 -0300

    Refactor in MainMenuView

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/Main.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/Main.java
@@ -2,22 +2,9 @@ package ar.fiuba.tdd.grupo10.nikoligames;
 
 import ar.fiuba.tdd.grupo10.nikoligames.uidelegate.views.MainMenuView;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        showMenu();
+        new MainMenuView();
     }
-
-    private static void showMenu() {
-        JFrame frame = new JFrame("Nikoli Games");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JComponent newContentPane = new MainMenuView();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
 
 }
--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/uidelegate/views/MainMenuView.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/uidelegate/views/MainMenuView.java
@@ -9,9 +9,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainMenuView extends JPanel {
+public class MainMenuView extends JFrame {
     private JComboBox gameCombo;
-    private JButton startButton;
 
     private enum GameEnum {
         SUDOKU,
@@ -19,25 +18,57 @@ public class MainMenuView extends JPanel {
     }
 
     public MainMenuView() {
+        gameCombo = createGameCombo();
+
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        JLabel selectionLabel = new JLabel("Seleccione un juego:");
+        setContentPane(createMainPanel());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        setTitle("Nikoli Games");
+        setVisible(true);
+    }
 
-        gameCombo = new JComboBox(GameEnum.values());
-        gameCombo.setEditable(false);
+    private JPanel createMainPanel() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel,
+                BoxLayout.PAGE_AXIS));
+        mainPanel.add(createComboPanel());
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        mainPanel.add(createButtonPanel());
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        return mainPanel;
+    }
 
+    private JPanel createComboPanel() {
         JPanel comboPanel = new JPanel();
         comboPanel.setLayout(new BoxLayout(comboPanel,
                 BoxLayout.PAGE_AXIS));
-        comboPanel.add(selectionLabel);
-        gameCombo.setAlignmentX(Component.LEFT_ALIGNMENT);
+        comboPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        comboPanel.add(new JLabel("Seleccione un juego:"));
         comboPanel.add(gameCombo);
+        return comboPanel;
+    }
 
+    private JPanel createButtonPanel() {
         JPanel buttonPanel = new JPanel(new GridLayout(0, 1));
-        startButton = new JButton("Jugar");
+        buttonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        buttonPanel.add(createStartButton());
+        return buttonPanel;
+    }
+
+    private JComboBox createGameCombo() {
+        JComboBox gameCombo = new JComboBox(GameEnum.values());
+        gameCombo.setEditable(false);
+        gameCombo.setAlignmentX(Component.LEFT_ALIGNMENT);
+        return gameCombo;
+    }
+
+    private JButton createStartButton() {
+        JButton startButton = new JButton("Jugar");
         startButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent event) {
                 GameEnum game = (GameEnum) gameCombo.getSelectedItem();
                 if (GameEnum.SUDOKU.equals(game)) {
                     new SudokuController(SudokuFactory.createFromScratch(70));
@@ -46,16 +77,7 @@ public class MainMenuView extends JPanel {
                 }
             }
         });
-        buttonPanel.add(startButton);
-
-        comboPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        buttonPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-
-        add(comboPanel);
-        add(Box.createRigidArea(new Dimension(0, 20)));
-        add(buttonPanel);
-
-        setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
+        return startButton;
     }
 
     private void showGameUnavailableDialog() {
