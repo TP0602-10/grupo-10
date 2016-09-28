2644c198008039c0dec96a459645c71d9ffffe44
FacuEt <facurawson@gmail.com>
2016-09-28 12:21:35 -0300

    fix bug in MutableCell

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/cells/MutableCell.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/grid/cells/MutableCell.java
@@ -1,5 +1,6 @@
 package ar.fiuba.tdd.grupo10.nikoligames.grid.cells;
 
+import ar.fiuba.tdd.grupo10.nikoligames.exceptions.ImmutableCellException;
 import ar.fiuba.tdd.grupo10.nikoligames.exceptions.ImmutableContentValueException;
 import ar.fiuba.tdd.grupo10.nikoligames.exceptions.NoFindContentbyTagException;
 import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.Content;
@@ -28,12 +29,21 @@ public class MutableCell extends Cell{
 
     @Override
     public void setValue(Object value, String contentTag) throws ImmutableContentValueException, NoFindContentbyTagException {
-        Content content = contents.get(contentTag);
-        if (content != null) {
+        if (contents.containsKey(contentTag)) {
+            Content content = contents.get(contentTag);
             content.setValue(value);
         } else {
             throw new NoFindContentbyTagException("Cannot find content for the tag: " + contentTag);
         }
+    }
+
+    @Override
+    public void setValue(Object value) throws NoFindContentbyTagException, ImmutableCellException, ImmutableContentValueException {
+        if (contents.size() > 1) {
+            throw new NoFindContentbyTagException("Not specified tag, cell contain more than one content.");
+        }
+        Content content = contents.entrySet().iterator().next().getValue();
+        content.setValue(value);
     }
 
 
