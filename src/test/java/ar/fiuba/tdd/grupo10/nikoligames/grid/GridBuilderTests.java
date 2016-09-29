0df0f5d711e6904e6e67783fbc755a809f163557
FacuEt <facurawson@gmail.com>
2016-09-29 16:14:24 -0300

    Add test un gridBuilder

--- src/test/java/ar/fiuba/tdd/grupo10/nikoligames/grid/GridBuilderTests.java
+++ src/test/java/ar/fiuba/tdd/grupo10/nikoligames/grid/GridBuilderTests.java
@@ -3,9 +3,17 @@ package ar.fiuba.tdd.grupo10.nikoligames.grid;
 import ar.fiuba.tdd.grupo10.nikoligames.exceptions.WrongNumberOfGridCellsException;
 import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.Cell;
 import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.ImmutableCell;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.MutableCell;
 import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.ImmutableContent;
 
+import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.MutableContent;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.GridRule;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.GridRuleCondition;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.GridRuleIterator;
 import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.GridRuleManager;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.matchers.EqualsMatcher;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.operations.GridRuleOperation;
+import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.operations.SumOperation;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,9 +21,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class GridBuilderTests {
     private List<Cell> flattenCells;
@@ -63,8 +73,32 @@ public class GridBuilderTests {
     public void addObservers() {
         GridBuilder gridBuilder = new GridBuilder();
         Collection<OnGridUpdatedObserver> observers = new ArrayList<>();
+
         observers.add(new GridRuleManager());
         assertNotNull(gridBuilder.addObservers(observers));
+
+        final GridRuleOperation<Integer> operation = new SumOperation("TAG");
+        final GridRuleCondition<Integer> condition = new GridRuleCondition<>(
+                new EqualsMatcher<>(),
+                1
+        );
+
+        List<Cell> allCells = new ArrayList<>();
+        allCells.add(new MutableCell(new MutableContent<>(1, "TAG")));
+        allCells.add(new MutableCell(new MutableContent<>(2, "TAG")));
+        allCells.add(new MutableCell(new MutableContent<>(3, "TAG")));
+
+        GridRuleIterator simpleIterator = new GridRuleIterator(allCells,"Iterate over all cells");
+
+        GridRule gridRule = new GridRule<>(
+                simpleIterator,
+                operation,
+                condition
+        );
+
+        GridRuleManager gridRuleManager = new GridRuleManager();
+        assertTrue(gridRuleManager.addRule(gridRule));
+        assertTrue(gridRuleManager.removeRule(gridRule));
     }
 
 }
