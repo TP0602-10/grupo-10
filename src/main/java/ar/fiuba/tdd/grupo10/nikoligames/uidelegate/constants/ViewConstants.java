383d71f3d4fec7ffd8e5b7233ff3069751c5e990
ana1ia <analiacuenca@gmail.com>
2016-09-29 01:50:27 -0300

    Usando view constans

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/uidelegate/constants/ViewConstants.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/uidelegate/constants/ViewConstants.java
@@ -2,4 +2,5 @@ package ar.fiuba.tdd.grupo10.nikoligames.uidelegate.constants;
 
 public class ViewConstants {
     public static final String TITLE = "Nikoli Games";
+    public static final int ROW_HEIGHT_DEFAULT = 50;
 }
