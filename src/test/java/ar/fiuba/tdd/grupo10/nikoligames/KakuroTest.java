7f113929402c15a1c6f2e4a19965828b31778017
Ezequiel <ezequielsantam@gmail.com>
2016-09-29 20:53:11 -0300

    Se modifica KakuroTest para eliminar warning

--- src/test/java/ar/fiuba/tdd/grupo10/nikoligames/KakuroTest.java
+++ src/test/java/ar/fiuba/tdd/grupo10/nikoligames/KakuroTest.java
@@ -25,25 +25,25 @@ public class KakuroTest {
         Assert.assertEquals(kakuroGrid.getCellAt(0, 0).getContent().getValue(), null);
         Assert.assertEquals(kakuroGrid.getCellAt(0, 2).getContent().getValue(), 16);
         Assert.assertEquals(kakuroGrid.getCellAt(0, 6).getContent().getValue(), 6);
-        kakuroGrid.getCellAt(1, 2).getContent().setValue(5);
+        kakuroGrid.getCellAt(1, 2).setValue(5);
         Assert.assertEquals(kakuroGrid.getCellAt(1, 2).getContent().getValue(), 5);
     }
 
     @Test
     public void playCompleteKakuroTestGame() {
         Grid kakuroGrid = KakuroFactory.createGrid(2);
-        kakuroGrid.getCellAt(1, 1).getContent().setValue(9);
-        kakuroGrid.getCellAt(1, 2).getContent().setValue(7);
-        kakuroGrid.getCellAt(1, 5).getContent().setValue(3);
-        kakuroGrid.getCellAt(1, 6).getContent().setValue(1);
-        kakuroGrid.getCellAt(2, 1).getContent().setValue(8);
-        kakuroGrid.getCellAt(2, 2).getContent().setValue(9);
-        kakuroGrid.getCellAt(2, 3).getContent().setValue(6);
-        kakuroGrid.getCellAt(2, 5).getContent().setValue(9);
-        kakuroGrid.getCellAt(2, 6).getContent().setValue(5);
-        kakuroGrid.getCellAt(2, 7).getContent().setValue(7);
-        kakuroGrid.getCellAt(2, 8).getContent().setValue(9);
+        kakuroGrid.getCellAt(1, 1).setValue(9);
+        kakuroGrid.getCellAt(1, 2).setValue(7);
+        kakuroGrid.getCellAt(1, 5).setValue(3);
+        kakuroGrid.getCellAt(1, 6).setValue(1);
+        kakuroGrid.getCellAt(2, 1).setValue(8);
+        kakuroGrid.getCellAt(2, 2).setValue(9);
+        kakuroGrid.getCellAt(2, 3).setValue(6);
+        kakuroGrid.getCellAt(2, 5).setValue(9);
+        kakuroGrid.getCellAt(2, 6).setValue(5);
+        kakuroGrid.getCellAt(2, 7).setValue(7);
+        kakuroGrid.getCellAt(2, 8).setValue(9);
         Assert.assertEquals(kakuroGrid.isComplete(), Boolean.FALSE);
-        kakuroGrid.getCellAt(4, 8).getContent().setValue(6);
+        kakuroGrid.getCellAt(4, 8).setValue(6);
     }
 }
