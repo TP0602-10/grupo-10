c78930ce6180bcee148748f28defeb3fc0568458
martin <martin@magnetico.com.ar>
2016-10-27 10:38:34 -0300

    Checkstyle fix

--- src/main/java/ar/fiuba/tdd/grupo10/nikoligames/helpers/ClassPathHelper.java
+++ src/main/java/ar/fiuba/tdd/grupo10/nikoligames/helpers/ClassPathHelper.java
@@ -1,72 +1,64 @@
 package ar.fiuba.tdd.grupo10.nikoligames.helpers;
 
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.Cell;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.ImmutableContainer;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.MutableContainer;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.ImmutableContent;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.MutableContent;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.types.NullValue;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.types.Number;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.types.Point;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.types.StringValue;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.cells.content.types.line.*;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.AlwaysVerifiableRule;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.CompleteIteratorRule;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.matchers.EqualsMatcher;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.matchers.GreaterThanIntegerMatcher;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.matchers.LessOrEqualToIntegerMatcher;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.matchers.LessThanIntegerMatcher;
-import ar.fiuba.tdd.grupo10.nikoligames.grid.rules.operations.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 
 public class ClassPathHelper {
 
     private static final Map<String,String> classesPackage = new HashMap<>();
 
-    private static final Class[] classesName = {
-            Cell.class,
-            ImmutableContainer.class,
-            MutableContainer.class,
-            ImmutableContent.class,
-            MutableContent.class,
-            DistinctOperation.class,
-            LineCircuitOperation.class,
-            SumLinesPointingToSharedCornerOperation.class,
-            EqualsMatcher.class,
-            LessOrEqualToIntegerMatcher.class,
-            LessThanIntegerMatcher.class,
-            AlwaysVerifiableRule.class,
-            CompleteIteratorRule.class,
-            CountFilledOperation.class,
-            SumOperation.class,
-            GreaterThanIntegerMatcher.class,
-            Number.class,
-            NullValue.class,
-            //StringValue.class,
-            VerticalLine.class,
-            HorizontalLine.class,
-            FromTopToRightLine.class,
-            FromBottomToLeftLine.class,
-            //FromTopLeftToBottomRightDiagonal.class,
-            FromBottomToRightLine.class,
-            FromTopToLeftLine.class,
-            //FromBottomLeftToTopRightDiagonal.class,
-            Point.class,
-            CheckTopBorderOperation.class,
-            CheckBottomBorderOperation.class,
-            CheckLeftBorderOperation.class,
-            CheckRightBorderOperation.class
-    };
-
     static {
-        for (Class cl : classesName) {
-            classesPackage.put(cl.getSimpleName(),cl.getPackage().getName());
+        try {
+            for (Class cl : getClasses("ar.fiuba.tdd.grupo10.nikoligames")) {
+                classesPackage.put(cl.getSimpleName(),cl.getPackage().getName());
+            }
+        } catch  (Exception exc) {
+            exc.printStackTrace();
         }
     }
 
     public static Map<String,String> getClasesPath() {
         return classesPackage;
+    }
+
+    private static Class[] getClasses(String packageName)
+            throws ClassNotFoundException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        assert classLoader != null;
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        ArrayList<Class> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        return classes.toArray(new Class[classes.size()]);
+    }
+
+    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        addClasses(classes, directory, packageName);
+        return classes;
+    }
+
+    private static void addClasses(List<Class> classes, File directory, String packageName) throws ClassNotFoundException {
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                assert !file.getName().contains(".");
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
     }
 }
